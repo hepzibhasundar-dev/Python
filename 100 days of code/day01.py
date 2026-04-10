9554e5d618f389cdb7a0c03b974635378efe0d74
HepzibhaSundararaj <{hepzibha.sundar@gmail.com}>
2026-04-09 23:44:06 -0700

    print command

--- 100 days of code/day01.py
+++ 100 days of code/day01.py
@@ -1,2 +1,12 @@
 # Day one project
 print("Hello world!"); #String of characters
+# Day one project
+print("Hello world!"); #String of characters
+# Mix 500g of flour, 10g Yeast and 300ml of water in a bowl.
+# Knead the dough for 10 minutes
+# Add 3g of salt
+# Leave to rise for 2hours.
+#Bake at 200 degress for 30 minutes
+
+#String manipulation
+print("1. Mix 500g of flour, 10g Yeast and 300ml of water in a bowl.\n2. Knead the dough for 10 minutes\n3. Add 3g of salt\n4. Leave to rise for 2hours.\n5.Bake at 200 degress for 30 minutes")
