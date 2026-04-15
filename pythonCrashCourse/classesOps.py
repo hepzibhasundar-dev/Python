d62a23cd8318647012742b432897a966dbe30642
HepzibhaSundararaj <{hepzibha.sundar@gmail.com}>
2026-04-14 22:10:40 -0700

    classes init method

---
+++ pythonCrashCourse/classesOps.py
@@ -0,0 +1,16 @@
+#Creating and using a class
+#Creating the Dog class
+class Dog:
+    """A simple to model a dog."""
+    def __init__(self, name, age):
+        """Initialize name and age attributes."""
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        """Simulate a dog sitting in response to a command."""
+        print(f"{self.name} is now sitting")
+
+    def roll_over(self):
+        """Simulate rolling over in response to a command."""
+        print(f"{self.name} rolled over")
