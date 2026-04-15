c68f7caed22c9b11c42cca8e1b7e863cc889109a
HepzibhaSundararaj <{hepzibha.sundar@gmail.com}>
2026-04-14 22:11:10 -0700

    class dog class

--- pythonCrashCourse/classesOps.py
+++ pythonCrashCourse/classesOps.py
@@ -14,3 +14,14 @@ class Dog:
     def roll_over(self):
         """Simulate rolling over in response to a command."""
         print(f"{self.name} rolled over")
+
+#The __init__() method
+#Making an Instance from a class
+class Dog:
+    --snip--
+
+my_dog = Dog('Willie', 6)
+
+print(f"My dog's name is (my_dog.name)")
+print(f"My dog is {my_dog.age} years old.")
+
